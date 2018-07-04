608bdc092bc66e58f3e80fd1190c41c64bcc2645
Adam Ladachowski <ladachowski@cic.com>
2018-07-04 16:59:31 +0200

    spec correction

--- spec/iml_spec.rb
+++ spec/iml_spec.rb
@@ -7,41 +7,41 @@ RSpec.describe IML do
       expect(IML::Base.new).to be_an(IML::Base)
     end
   end
-  context 'String' do
+  context 'Text' do
     it '#new initializes a new object' do
-      expect(IML::String.new).to be_an(IML::String)
+      expect(IML::Text.new).to be_an(IML::Text)
     end
 
     it '#movie? returns a positive result' do
       title = 'Snowblind.2010.720p.BluRay.H264.AAC-RARBG.mp4'
-      expect(IML::String.new(title).movie?).to be_an(IML::Movie)
+      expect(IML::Text.new(title).movie?).to be_an(IML::Movie)
     end
 
     it '#movie? returns a negative result' do
       title = 'foobar foobar'
-      expect(IML::String.new(title).movie?).to be(false)
+      expect(IML::Text.new(title).movie?).to be(false)
     end
 
     it '#tv? returns a positive result' do
       title = 'Walk.the.Prank.S03E09.WEBRip.x264-ION10.mkv'
-      expect(IML::String.new(title).tv?).to be_an(IML::TVSeries)
+      expect(IML::Text.new(title).tv?).to be_an(IML::TVSeries)
     end
 
     it '#tv? returns a negative result' do
       title = 'foobar foobar'
-      expect(IML::String.new(title).tv?).to be(false)
+      expect(IML::Text.new(title).tv?).to be(false)
     end
   end
   context 'Movie' do
     it '#movie? returns a positive result' do
       title = 'Snowblind.2010.720p.BluRay.H264.AAC-RARBG.mp4'
-      expect(IML::String.new(title).detect.movie?).to be(true)
+      expect(IML::Text.new(title).detect.movie?).to be(true)
     end
   end
   context 'TVSeries' do
     it '#tv? returns a positive result' do
       title = 'Walk.the.Prank.S03E09.WEBRip.x264-ION10.mkv'
-      expect(IML::String.new(title).detect.tv?).to be(true)
+      expect(IML::Text.new(title).detect.tv?).to be(true)
     end
   end
 end
